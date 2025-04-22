404c74fe1ae9d3ea957391b1e6a9139b99b7f9be
harshitit112 <hv702383@gmail.com>
2025-04-22 19:31:29 +0530

    this is playbar and hero section of the clone

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 import React from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
 
 function App() {
   return (
-    <div>
-    
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar />
+        <Display />
+      </div>
+      <Player />
     </div>
   )
 }
