d520cd05beaf52edae4bfa700070e97acb02b485
harshitit112 <hv702383@gmail.com>
2025-04-24 16:32:04 +0530

    add navigation to the site and update the status bar and time status are also update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
-import React from 'react'
+import React, { useContext } from 'react'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
+
 
 function App() {
+
+  const {audioRef,track} = useContext(PlayerContext)
+  
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
@@ -12,6 +17,7 @@ function App() {
         <Display />
       </div>
       <Player />
+      <audio ref = {audioRef} src={track.file} preload='auto'></audio>
     </div>
   )
 }
--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
 import { albumsData, assets, songsData } from '../assets/assets';
 import AlbumItem from './AlbumItem';
+import { PlayerContext } from '../context/PlayerContext';
 
 const DisplayAlbum = () => {
 
     const {id} = useParams();
     
     const ablumData = albumsData[id];
+
+    const {playWithId} = useContext(PlayerContext);
     
     
     
@@ -39,7 +42,7 @@ const DisplayAlbum = () => {
     <hr />
     {
         songsData.map((item, index) => (
-            <div key={index} className='grid grid-col-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
+            <div onClick={() => playWithId(item.id)} key={index} className='grid grid-col-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
                 <p className='text-white'>
                     <b className='mr-4 text-[#a7a7a7]'>{index+1}</b>
                     <img className='inline w-10 me-5' src={item.image} alt="" />
